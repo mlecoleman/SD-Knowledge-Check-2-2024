a7b691e4fe9725c18b0bafc81f63da7d3122f4ce
E.M. Coleman <mlecoleman@gmail.com>
2024-05-10 19:58:17 -0400

    Update Program.cs and classes for knowledge check 2

---
+++ KnowledgeCheckTwo/Program.cs
@@ -0,0 +1,31 @@
+﻿
+using KnowledgeCheckTwo;
+
+Console.WriteLine("How many records do you want to add? ");
+var numberOfRecords = int.Parse(Console.ReadLine());
+
+var recordList = new List<Bicycle>();
+for (int i = 0; i < numberOfRecords; i++)
+{
+    // In this loop, populate the object's properties using Console.ReadLine()
+    var bicycle = new Bicycle();
+
+    Console.WriteLine("What is the make of the bicycle?");
+    bicycle.Make = Console.ReadLine();
+
+    Console.WriteLine("Enter the value for number of tires.");
+    bicycle.NumberOfTires = int.Parse(Console.ReadLine());
+
+    Console.WriteLine("Enter the value for number of riders.");
+    bicycle.NumberOfRiders = int.Parse(Console.ReadLine());
+
+    recordList.Add(bicycle);
+}
+
+foreach (Bicycle bicycle in recordList)
+{
+    Console.WriteLine($"Bicycle of Make: {bicycle.Make} with {bicycle.NumberOfTires} Tire(s) and a rider capacity of {bicycle.NumberOfRiders} ");
+}
+
+Console.ReadKey();
+
